33f61bc2a994ca4584e1edd34ccea02cc8fe3880
Fajar AM <somabangsa@gmail.com>
2022-03-08 20:54:30 +0700

    add builddirectory

---
+++ app/generalSettings.js
@@ -0,0 +1,14 @@
+const generalSetting = {
+        
+   
+    "uiSetting": {
+        "defaultTitle": "Kafila Resource Management"
+    },
+     
+    "hosts":{
+        "api": "https://kafilaresourcemanagement.herokuapp.com",
+        
+    },
+    
+   
+}
--- public/generalSettings.js
+++ public/generalSettings.js
@@ -6,7 +6,8 @@ const generalSetting = {
     },
      
     "hosts":{
-        "api": "http://127.0.0.1:8080/resourcemanagement",
+        "_api": "http://127.0.0.1:8080/resourcemanagement",
+        "api": "https://kafilaresourcemanagement.herokuapp.com",
         
     },
     
