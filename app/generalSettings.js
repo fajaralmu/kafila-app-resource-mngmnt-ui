7dd19778173efcffe40762c65b8ef64e88412ce4
Fajar AM <somabangsa@gmail.com>
2022-03-16 22:04:14 +0700

    build

--- app/generalSettings.js
+++ app/generalSettings.js
@@ -6,6 +6,7 @@ const generalSetting = {
     },
      
     "hosts":{
+        "_api": "http://127.0.0.1:8080/resourcemanagement",
         "api": "https://kafilaresourcemanagement.herokuapp.com",
         
     },
