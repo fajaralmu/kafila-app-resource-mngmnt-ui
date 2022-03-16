1c307d42da273654ac4e083510eff487ed87ab22
Fajar AM <somabangsa@gmail.com>
2022-03-16 23:40:03 +0700

    change api host

--- app/generalSettings.js
+++ app/generalSettings.js
@@ -6,8 +6,8 @@ const generalSetting = {
     },
      
     "hosts":{
-        "api": "http://127.0.0.1:8080/resourcemanagement",
-        "_api": "https://kafilaresourcemanagement.herokuapp.com",
+        "_api": "http://127.0.0.1:8080/resourcemanagement",
+        "api": "https://kafilaresourcemanagement.herokuapp.com",
         
     },
     
