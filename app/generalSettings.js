d4cc707bd7c92e428c95468d112381c5e42f3d9c
Fajar Kartawireja <44136910+fajaralmu@users.noreply.github.com>
2022-04-16 23:44:30 +0700

    set temporary API path

--- app/generalSettings.js
+++ app/generalSettings.js
@@ -7,7 +7,7 @@ const generalSetting = {
      
     "hosts":{
         "_api": "http://127.0.0.1:8080/kafila-app-master",
-        "api": "https://18.136.218.152:8080/kafila-app-master",
+        "api": "https://18.136.218.152:8080/kafila-app-tahfiz",
         
     },
     
