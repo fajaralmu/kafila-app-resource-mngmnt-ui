bd4aa870b06d1afd2959742a126bce4251489d19
Fajar AM <somabangsa@gmail.com>
2022-08-08 21:45:12 +0700

    change IP

--- public/generalSettings.js
+++ public/generalSettings.js
@@ -7,7 +7,6 @@ const generalSetting = {
      
     "hosts":{
         "api": "http://127.0.0.1:8080/kafila-app-tahfiz",
-        "_api": "https://18.136.218.152.com/kafila-app-master",
         
     },
     
