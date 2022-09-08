6c1bc5854b04b3fef00976ea19ba0a49b19d9b8f
Fajar Kartawireja <44136910+fajaralmu@users.noreply.github.com>
2022-09-08 09:56:54 +0700

    Update generalSettings.js

--- app/generalSettings.js
+++ app/generalSettings.js
@@ -6,7 +6,7 @@ const generalSetting = {
     },
      
     "hosts":{
-        "api": "https://13.250.31.197:8080/kafila-app-tahfiz",
+        "api": "https://api.kafila.app/kafila-app-tahfiz",
     },
     
    
