26dc51ba03dcf097fba8565eb56f3655b3b471f2
Fajar AM <somabangsa@gmail.com>
2022-10-31 20:09:46 +0700

    Users page: enable authorities filter

--- src/pages/admin/masterData/UsersPage.tsx
+++ src/pages/admin/masterData/UsersPage.tsx
@@ -30,14 +30,14 @@ class UsersPage extends BaseMasterDataPage<User, BaseProps, State> {
       new DataTableHeaderValue("birthDate", "Date of Birth"),
       new DataTableHeaderValue("birthPlace", "Place of Birth"),
       new DataTableHeaderValue("gender", "Gender"),
-      new DataTableHeaderValue("role", "Role", false),
+      new DataTableHeaderValue("authorities.name", "Role"),
       new DataTableHeaderValue("phoneNumber", "Phone"),
     ];
   }
   resetPassword = (item: User) => {
     this.patchAction(item, ACTION_RESET_PASSWORD);
   }
-  render(): ReactNode {
+  render() {
 
     if (this.state.showForm && this.state.item) {
       return (
@@ -54,53 +54,55 @@ class UsersPage extends BaseMasterDataPage<User, BaseProps, State> {
       <ViewTemplate title={this.title} back="/admin">
         {result === undefined || items === undefined ?
           <i>Loading...</i> :
-          <form onSubmit={this.loadFromForm} className="mt-5 pl-3 pr-3" style={{ overflow: 'auto' }}>
+          <div className="mt-5 pl-3 pr-3" style={{ overflow: 'auto' }}>
             {this.paginationButton}
-            <table className="commonDataTable table table-striped">
-              <thead>
-                <tr>
-                  <th>No</th>
-                  {this.getDataTableHeaderComponent()}
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {items.map((item, i) => {
-                  return (
-                    <tr key={"user-" + item.id}>
-                      <td>{this.startingNumber + i}</td>
-                      <td>{item.email}</td>
-                      <td>{item.username}</td>
-                      <td>{item.fullName}</td>
-                      <td>{item.displayName}</td>
-                      <td>{new Date(item.birthDate).toLocaleDateString()}</td>
-                      <td>{item.birthPlace}</td>
-                      <td>{item.gender}</td>
-                      <td>
-                        {item.authorities.map(a => <div className="badge bg-success  me-1">{a.name?.toLocaleLowerCase()}</div>)}
-                      </td>
-                      <td>{item.phoneNumber}</td>
+            <form onSubmit={this.loadFromForm}>
+              <table className="commonDataTable table table-striped">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    {this.getDataTableHeaderComponent()}
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item, i) => {
+                    return (
+                      <tr key={"user-" + item.id}>
+                        <td>{this.startingNumber + i}</td>
+                        <td>{item.email}</td>
+                        <td>{item.username}</td>
+                        <td>{item.fullName}</td>
+                        <td>{item.displayName}</td>
+                        <td>{new Date(item.birthDate).toLocaleDateString()}</td>
+                        <td>{item.birthPlace}</td>
+                        <td>{item.gender}</td>
+                        <td>
+                          {item.authorities.map(a => <div key={`${item.id}-${a.name}`} className="badge bg-success me-1">{a.name?.toLocaleLowerCase()}</div>)}
+                        </td>
+                        <td>{item.phoneNumber}</td>
 
-                      <td>
-                        <div
-                          className="d-flex"
-                          style={{ flexWrap: 'nowrap', width: '150px' }}>
-                          <ActionButton
-                            iconClass="fas fa-lock-open"
-                            className="btn btn-warning btn-sm me-2"
-                            onClick={() => this.resetPassword(item)}
-                          >
-                          </ActionButton>
-                          {this.actionButton(item)}
-                        </div>
-                      </td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-              {this.tableFooter}
-            </table>
-          </form>}
+                        <td>
+                          <div
+                            className="d-flex"
+                            style={{ flexWrap: 'nowrap', width: '150px' }}>
+                            <ActionButton
+                              iconClass="fas fa-lock-open"
+                              className="btn btn-warning btn-sm me-2"
+                              onClick={() => this.resetPassword(item)}
+                            >
+                            </ActionButton>
+                            {this.actionButton(item)}
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+                {this.tableFooter}
+              </table>
+            </form>
+          </div>}
       </ViewTemplate>
     )
   }
