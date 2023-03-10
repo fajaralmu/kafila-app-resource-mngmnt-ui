f661295663d9496ec8280569f761ebe3627c45b9
Fajar AM <somabangsa@gmail.com>
2023-03-10 22:18:39 +0700

    full calendar component data types improvement

--- src/components/fullCalendar/FullCalendar.tsx
+++ src/components/fullCalendar/FullCalendar.tsx
@@ -24,51 +24,55 @@ const timeLineConstant = {
 let idSeq = 0;
 const uniqueId = () => new Date().getTime().toString() + (++idSeq);
 
+type Week = {
+  dayOfWeek: number,
+  week: number,
+  dayOfMonth: number,
+  now: boolean,
+}
 type State = {
   selectedMonth: number,
   selectedYear: number,
 }
 
 export default class FullCalendar extends ControlledComponent<any, State> {
-  private calendarData: any[];
+  private calendarData: Week[];
   private days = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Ahad"];
 
   private currentMonth: number;
   private currentYear: number;
 
-  private begin = { week: 1, day: 3, dayCount: 31, info: "" };
-  private begin_old = { week: 0, day: 0, dayCount: 0, info: "" };
+  private begin = { week: 1, day: 3, dayCount: 31, info: '' };
+  private begin_old = { week: 0, day: 0, dayCount: 0, info: '' };
 
-  private running_month: number;
-  private running_year: number;
+  private runningMonth: number;
+  private runningYear: number;
 
   constructor(props: any) {
     super(props);
+    const now = new Date();
     this.state = {
-      selectedMonth: new Date().getMonth() + 1,
-      selectedYear: new Date().getFullYear(),
-    }
+      selectedMonth: now.getMonth(),
+      selectedYear: now.getFullYear(),
+    };
 
     this.calendarData = [];
     this.currentMonth = 7;// 0; 
     this.begin = { week: 1, day: 3, dayCount: 31, info: "" };
     this.begin_old = { week: 0, day: 0, dayCount: 0, info: "" };
     this.currentYear = 1945;
-    this.running_month = 7;
-    this.running_year = 1945;
-  }
-  get date_info() {
-    return document.getElementById("date-info") as HTMLInputElement;
+    this.runningMonth = 7;
+    this.runningYear = 1945;
   }
   componentDidMount() {
     this.loadCalendar();
   }
   createTable = () => {
     //console.log("BUAT this.tabel"); 
-    let calendarData = [];
+    let calendarData: Week[] = [];
     for (let r = 1; r <= 6; r++) {
       for (let i = 1; i <= 7; i++) {
-        calendarData.push({ day: i, week: r });
+        calendarData.push({ dayOfWeek: i, week: r, dayOfMonth: NaN, now: false });
       }
     }
     this.calendarData = calendarData;
@@ -89,7 +93,6 @@ export default class FullCalendar extends ControlledComponent<any, State> {
     this.begin = this.fillDay(this.currentMonth, this.begin);
 
     this.setCalendar();
-    this.setState({ selectedMonth: new Date().getMonth() + 1, selectedYear: new Date().getFullYear() });
   }
   setCalendar = () => {
     this.doSetCalendar();
@@ -97,38 +100,39 @@ export default class FullCalendar extends ControlledComponent<any, State> {
   doSetCalendar = () => {
     console.log('--Start--');
 
-    this.running_month = this.state.selectedMonth;
-    this.running_year = this.state.selectedYear ? this.state.selectedYear : new Date().getFullYear();
-    let diff_year = Math.abs(this.running_year - this.currentYear);
-    // alert("diff_year year:" + diff_year);
+    this.runningMonth = this.state.selectedMonth;
+    this.runningYear = this.state.selectedYear ?? new Date().getFullYear();
+    let diff_year = Math.abs(this.runningYear - this.currentYear);
+
     let monthCount = 0;
     if (diff_year > 0)
-      monthCount = (11 - this.currentMonth) + (diff_year > 1 ? ((diff_year - 1) * 12) : 0) + (+this.running_month);
+      monthCount = Math.abs((11 - this.currentMonth) + (diff_year > 1 ? ((diff_year - 1) * 12) : 0) + (+this.runningMonth));
     else
-      monthCount = this.running_month - this.currentMonth;
+      monthCount = Math.abs(this.runningMonth - this.currentMonth);
+
     let less = false;
-    if (this.running_year - this.currentYear > 0) {
+    if (this.runningYear - this.currentYear > 0) {
       less = false;
-    } else if (this.running_year - this.currentYear < 0) {
+    } else if (this.runningYear - this.currentYear < 0) {
       less = true;
     } else {
-      if (this.running_month - this.currentMonth > 0) {
+      if (this.runningMonth - this.currentMonth > 0) {
         less = false;
       } else {
         less = true;
       }
     }
-    monthCount = Math.abs(monthCount);
     //console.log("kurang dari: ", less);
     let current_month = this.currentMonth;
     let endMonth = (monthCount + this.currentMonth);
+
     if (monthCount <= 0)
       return;
+
     if (!less) {
       for (let m = this.currentMonth + 1; m <= endMonth + 1; m++) {
         if (current_month > 11) {
           current_month = 0;
-
         }
         this.currentMonth = current_month;
         let end = this.nextMonth();
@@ -139,52 +143,41 @@ export default class FullCalendar extends ControlledComponent<any, State> {
         current_month++;
       }
     } else if (less) {
-      let pastMonthCount = (this.currentMonth) + (diff_year > 1 ? ((diff_year - 1) * 12) : 0) + (11 - this.running_month);
+      let pastMonthCount = (this.currentMonth) + (diff_year > 1 ? ((diff_year - 1) * 12) : 0) + (11 - this.runningMonth);
       endMonth = (pastMonthCount + this.currentMonth);
       //console.log("month now", this.month_now, "diff_year", monthCount, "from", to);
-      let begin_month = this.currentMonth;
+      const begin_month = this.currentMonth;
       for (let b = endMonth + 1; b >= begin_month + 1; b--) {
         if (current_month < 0) {
           current_month = 11;
         }
         this.currentMonth = current_month;
-        let end = this.prevMonth();
+        const end = this.prevMonth();
         if (end) {
           break;
         }
-        ////console.log("b",b,"month",current_month);
         current_month--;
       }
     }
-
-    this.fillInfo();
-    console.log("==end==")
-  }
-  fillInfo = () => {
-    if (this.date_info)
-      this.date_info.value = timeLineConstant.month[this.currentMonth].name + " " + this.currentYear;
-    this.refresh();
+    console.log("--End--")
   }
   detail = (day: number, month: number, year: number) => {
     if (this.props.detail) {
       this.props.detail(day, month, year);
     }
   }
-  prevMonth = () => {
-    // this.forceUpdate();
-    return this.doPrevMonth(false);
-  }
+  prevMonth = () => this.doPrevMonth(false);
   doPrevMonth = (prev: boolean) => {
     this.currentMonth--;
     if (prev) {
-      this.running_month--;
+      this.runningMonth--;
     }
     if (this.currentMonth < 0) {
       this.currentMonth = 11;
       this.currentYear--;
       if (prev) {
-        this.running_month = 11;
-        this.running_year--;
+        this.runningMonth = 11;
+        this.runningYear--;
       }
     }
     let begin_prev = this.findBegin(this.begin_old, this.begin_old.dayCount);
@@ -203,7 +196,7 @@ export default class FullCalendar extends ControlledComponent<any, State> {
       info: '',
     }
     this.refresh();
-    return switch_.info == "NOW";
+    return switch_.info === 'NOW';
   }
 
   nextMonth = () => {
@@ -221,18 +214,18 @@ export default class FullCalendar extends ControlledComponent<any, State> {
     console.log("NEXT")
     this.currentMonth++;
     if (next) {
-      this.running_month++;
+      this.runningMonth++;
     }
     if (this.currentMonth > 11) {
       this.currentMonth = 0;
       this.currentYear++;
       if (next) {
-        this.running_month = 0;
-        this.running_year++;
+        this.runningMonth = 0;
+        this.runningYear++;
       }
     }
 
-    let switch_ = this.fillDay(this.currentMonth, this.begin);
+    const switch_ = this.fillDay(this.currentMonth, this.begin);
     this.begin_old = {
       week: this.begin.week,
       day: this.begin.day,
@@ -246,7 +239,7 @@ export default class FullCalendar extends ControlledComponent<any, State> {
       info: '',
     }
     this.refresh();
-    return switch_.info == "NOW";
+    return switch_.info == 'NOW';
 
   }
 
@@ -276,29 +269,27 @@ export default class FullCalendar extends ControlledComponent<any, State> {
     return begin_prev_;
   }
 
-  setElementByAttr = (val: number, val2: number, day: number) => {
+  setElementByAttr = (week: number, dayOfWeek: number, dayOfMonth: number) => {
+    const now = new Date();
     for (let i = 0; i < this.calendarData.length; i++) {
       let data = this.calendarData[i];
 
-      if (data.week == val && data.day == val2) {
-        if (new Date().getDate() == day &&
-          new Date().getMonth() == this.currentMonth &&
-          new Date().getFullYear() == this.currentYear) {
+      if (data.week == week && data.dayOfWeek == dayOfWeek) {
+        if (now.getDate() == dayOfMonth && now.getMonth() == this.currentMonth && now.getFullYear() == this.currentYear) {
 
-          console.log("NOW", i);
+          console.log('NOW -> ', data);
           this.calendarData[i].now = true;
         } else {
-          //   console.log("NOT NOW", i);
           this.calendarData[i].now = false;
         }
-        this.calendarData[i].text = day;
+        this.calendarData[i].dayOfMonth = dayOfMonth;
       }
     }
   }
 
   clear = () => {
     for (let i = 0; i < this.calendarData.length; i++) {
-      this.calendarData[i].text = "";
+      this.calendarData[i].dayOfMonth = NaN;
     }
     timeLineConstant.month[1].day = 28 + (+this.currentYear % 4 == 0 ? 1 : 0);
   }
@@ -311,34 +302,30 @@ export default class FullCalendar extends ControlledComponent<any, State> {
       dayCount: begin.dayCount,
       info: '',
     };
-    let week_ = begin_new.week;
+    let weekOfMonth = begin_new.week;
     if (begin_new.week > 1 && begin_new.day > 1) {
-      week_ = 1;
+      weekOfMonth = 1;
     }
-    let day_ = begin_new.day;
-    let isNow = this.running_month == current_month && this.running_year == this.currentYear;
-    //  console.log("isNow", isNow,this.running_month,'=',current_month, this.running_month == current_month,this.running_year,'=',this.year_now, this.running_year == this.year_now)
-    for (let d = 1; d <= timeLineConstant.month[current_month].day; d++) {
-      if (day_ > 7) {
-        day_ = 1;
-        week_++;
+    let dayOfWeek = begin_new.day;
+    let isNow = this.runningMonth == current_month && this.runningYear == this.currentYear;
+    const currentMonthDayCount = timeLineConstant.month[current_month].day;
+    for (let dayOfMonth = 1; dayOfMonth <= currentMonthDayCount; dayOfMonth++) {
+      if (dayOfWeek > 7) {
+        dayOfWeek = 1;
+        weekOfMonth++;
       }
       if (isNow) {
-        this.setElementByAttr(week_, day_, d);
+        this.setElementByAttr(weekOfMonth, dayOfWeek, dayOfMonth);
       }
-      day_++;
+      dayOfWeek++;
     }
-    begin_new.week = week_ >= 5 ? 2 : 1;
-    begin_new.day = day_;
-    begin_new.dayCount = timeLineConstant.month[current_month].day;
-    //console.log("old", begin_old_.day, begin_old_.week);
-    //console.log("   ");
-    //console.log("new", begin_new.day, begin_new.week);
-    this.fillInfo();
+    begin_new.week = weekOfMonth >= 5 ? 2 : 1;
+    begin_new.day = dayOfWeek;
+    begin_new.dayCount = currentMonthDayCount;
+
     if (isNow) {
-      // this.detail(null, (+this.running_month + 1), this.running_year);
-      begin_new.info = "NOW";
-      this.getEventByDate(this.running_month + 1, this.running_year);
+      begin_new.info = 'NOW';
+      this.getEventByDate(this.runningMonth + 1, this.runningYear);
     } else {
       begin_new.info = "SOME-DAY";
     }
@@ -346,56 +333,24 @@ export default class FullCalendar extends ControlledComponent<any, State> {
   }
 
   render() {
-    //validate division
     const { selectedYear } = this.state;
+    const { month } = timeLineConstant;
+    const calendarTitles = this.days.map((d) => <div key={`title-${d}`}>{d}</div>);
+    const dateInfoText = month[this.currentMonth].name + ' ' + this.currentYear;
 
-    let totalCalendarData = this.days.map(day => {
-      return ({ text: day, title: true, now: false })
-    })
-
-    this.calendarData.forEach(element => {
-      totalCalendarData.push(element);
-    });
-
-    const calendarData = totalCalendarData.map((data) => {
-      if (data.text == null || data.text == "") {
-        return <div key={uniqueId()}></div>
+    const calendarComponent = this.calendarData.map((data) => {
+      if (isNaN(data.dayOfMonth) || data.dayOfMonth < 0) {
+        return <area />;
       }
-      let style: React.CSSProperties = { width: '80%', minHeight: '150px', marginBottom: '15px' };
-      if (data.title == true) {
-        return (<div key={uniqueId()}>{data.text}</div>)
-      }
-      if (data.now == true) {
-        style = {
-          ...style,
-          backgroundColor: 'lightgreen'
-        }
-      }
-
-      const events = this.getEventDetail(+data.text);
-      const eventList = events.map((event, i) => {
-        if (i <= 3) {
-          return <p key={"EVT_" + i}>{event.name}</p>
-        } else if (i == 4) {
-          return <ActionButton key={uniqueId()} onClick={() => { }}>More</ActionButton>
-        } else {
-          return null;
-        }
-      });
-
       return (
-        <div key={uniqueId()} style={style}>
-          <p>{data.text}</p>
-          <button type="button" onClick={() => this.detail(parseInt(data.text), this.currentMonth, this.currentYear)}>
+        <div key={uniqueId()} className={`calendar-item mb-2 me-2 px-2 border border-dark ${data.now ? 'calendar-item-now' : ''}`}>
+          <h6>{data.dayOfMonth} {data.now ? '*' : ''}</h6>
+          <button type="button" onClick={() => this.detail(data.dayOfMonth, this.currentMonth, this.currentYear)}>
             Detail
           </button>
-          {eventList}
         </div>
       );
-    }
-    )
-
-    const dateInfoText = timeLineConstant.month[this.currentMonth].name + ' ' + this.currentYear;
+    });
     return (
       <div className="calendar-wrapper">
         <div className="period-selection mt-2 mb-2">
@@ -405,19 +360,17 @@ export default class FullCalendar extends ControlledComponent<any, State> {
             defaultValue={this.state.selectedMonth}
             onChange={this.handleInputChange}
           >
-            {timeLineConstant.month.map((m, i) => {
-              return <option key={m.name} value={i}>{m.name}</option>
-            })}
+            {month.map((m, i) => <option key={m.name} value={i}>{m.name}</option>)}
           </select>
-          <input className="form-control" type="number" name="inputYearValue" value={selectedYear} onChange={this.handleInputChange} />
+          <input className="form-control" type="number" name="selectedYear" value={selectedYear} onChange={this.handleInputChange} />
           <button type="button" className="btn btn-dark" onClick={this.setCalendar}>Go</button>
         </div>
         <div className="period-selection mb-5">
           <button type="button" className="btn btn-dark" onClick={(e) => this.doPrevMonth(true)}>Prev</button>
-          <input value={dateInfoText} className="form-control" id="date-info" disabled />
+          <h4 className="text-center">{dateInfoText}</h4>
           <button type="button" className="btn btn-dark" onClick={(e) => this.doNextMonth(true)}>Next</button>
         </div>
-        <GridComponent cols={7} items={calendarData} style={{}} />
+        <GridComponent cols={7} items={[...calendarTitles, ...calendarComponent]} style={{}} />
       </div>
     );
   }
